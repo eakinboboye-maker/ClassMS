6817a5221e0092984f4351d2e95fb96d6993b52e
EniolaAkin <eniola.akinboboye@gmial.com>
2026-05-11 13:14:01 +0100

    teacher dash

--- frontend/teacher-dashboard/.next/server/middleware-build-manifest.js
+++ frontend/teacher-dashboard/.next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-db0a529a99835594.js","static/chunks/4bd1b696-d7266ff7e407fd8a.js","static/chunks/517-0e6e05e9279f6d45.js","static/chunks/main-app-c7047859b2910303.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-db0a529a99835594.js","static/chunks/framework-d29117d969504448.js","static/chunks/main-ac97e502c60003f8.js","static/chunks/pages/_app-d23763e3e6c904ff.js"],"/_error":["static/chunks/webpack-db0a529a99835594.js","static/chunks/framework-d29117d969504448.js","static/chunks/main-ac97e502c60003f8.js","static/chunks/pages/_error-9b7125ad1a1e68fa.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
